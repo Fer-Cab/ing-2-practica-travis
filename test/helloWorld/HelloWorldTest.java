1457b37d8aa884b00d8bc12f9899f69c386d2dee
Fer-Cab <ferjcb@live.com>
2015-09-28 20:55:43 -0300

    Update HelloWorldTest.java

--- test/helloWorld/HelloWorldTest.java
+++ test/helloWorld/HelloWorldTest.java
@@ -10,8 +10,27 @@ public class HelloWorldTest {
 	
 	HelloWorld cronometro = new HelloWorld ();
 	@Test
-	public void testreiniciar(){
+	public void testReiniciar(){
 		cronometro.reiniciar();
 	assertEquals(0,cronometro.getSegundos() );
+	assertEquals(0,cronometro.getMinutos() );
 	}
+	
+	@Test
+	public void testIncrementarTiempo(){
+	for(i=0,i<61,i++){
+		cronometro.incrementarTiempo()
+	}
+	assertEquals(0,cronometro.getSegundos() );
+	assertEquals(1,cronometro.getMinutos() );
+	}
+	
+	@Test
+	public void testMostrarTiempo(){
+	
+	assertEquals("minutos = 1 segundos = 0",cronometro.mostrarTiempo);
+	}
+	
+
+	
 }
